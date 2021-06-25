ed4688a50c6077752b3944957c5a5fb42820cc9b
AlRiadHridoy <64416616+AlRiadHridoy@users.noreply.github.com>
2021-06-26 01:11:37 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,35 +2,47 @@
 const hamburger = document.querySelector('.toggle');
 const main = document.querySelector(".top-banner img");
 const nav = document.querySelector("nav ul")
+const navChild = document.querySelectorAll("nav ul li");
 const cards = document.querySelector('.pricing-cards');
 const miniCards = document.querySelector('.choice-cards');
 const header = document.querySelector('header');
 const banner = document.querySelector('.home-container');
 
-
-
-const temp = document.querySelector('.temp');
-
-
+//*************************** Menu Animation
+// Hmaburger Function
 let arr = ['top', 'middle', 'bottom'];
-
-//******** Menu Animation
-hamburger.addEventListener('click', function() {
+const hamburgerFunc = function() {
     i = 0;
     for (let element of hamburger.children) {
         element.classList.toggle(arr[i]);
         i ++;
-    }
+    };
+};
+
+hamburger.addEventListener('click', function() {
+    hamburgerFunc();
     nav.classList.toggle("toggle-left");
 });
+
+for (let element of nav.children) {
+    element.addEventListener("click", function(){
+        hamburgerFunc();
+        nav.classList.toggle("toggle-left");
+    });
+};
+
+navChild.forEach(ele => {
+    ele.addEventListener('click', function(){
+        navChild.forEach(li => li.classList.remove('temp'));
+        ele.classList.add('temp');
+    })
+})
 
 option = {
     rootMargin: '-150px'
 };
-
 const menuDown = new IntersectionObserver(function(entries, menuDown){
     entries.forEach(entry => {
-        console.log(entry)
         if (!entry.isIntersecting) {
             header.classList.add('header-clr');
         }
@@ -39,11 +51,9 @@ const menuDown = new IntersectionObserver(function(entries, menuDown){
         }
     });
 }, option);
-
 menuDown.observe(banner);
 
-
-//******** Banner Animation
+//*************************** Banner Animation
 const mainObserver = new IntersectionObserver(function(entries){
     entries.forEach(entry => {
         if (entry.isIntersecting) {
@@ -54,13 +64,11 @@ const mainObserver = new IntersectionObserver(function(entries){
 
 mainObserver.observe(main);
 
-
-//******** Cards/miniCards Animations
+//***************************  Cards/miniCards Animations
 const func = {
     treshold: 10,
     rootMargin: '150px',
 }
-
 var a = .1;
 const observer = new IntersectionObserver(function(entries){
     entries.forEach(entry => {
@@ -72,9 +80,7 @@ const observer = new IntersectionObserver(function(entries){
     a = .1;
 });
 
-
 let arrElement = [cards.children, miniCards.children];
-
 for (let child of arrElement) {
     for (let element of child) {
         observer.observe(element);
